b5caed5bef88057fb874c4d0db2e697cd11336db
thauanny <thauannyk@ufrn.edu.br>
2020-10-06 14:34:17 -0300

    Projeto final

---
+++ src/components/FormTodo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="form-todo form-group">
+        <p>
+            <input placeholder="nome" type="text" name="author" class="form-control" v-model="name">
+        </p>
+        <p>
+            <textarea name="message"  class="form-control" placeholder="Comentario*" v-model="message" required></textarea>
+        </p>
+            <button v-on:click="addComment" type="submit" class="btn btn-primary" >Comentar</button>
+        </div> 
+
+</template>
+
+<script>
+export default {
+
+     data(){ // data: function(){...}
+        return{
+            name: '',
+            message: ''
+        }
+    },
+    methods: {
+
+     addComment(){
+
+            if(this.message.trim() === ''){
+                alert("É preciso digitar uma mensagem!")
+                
+            }else{
+
+                this.$emit('add-todo',{
+                    name: this.name,
+                    message: this.message
+                });
+            } 
+
+            this.name = ''
+            this.message = ''
+        }
+    }
+}
+</script>
